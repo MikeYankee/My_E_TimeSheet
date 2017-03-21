2014820b15a0e968bfb66c95422c22c9aab8201d
Zenoko <yann.bourgues@hotmail.fr>
2017-03-21 15:35:59 +0100

    début fonctionnalité ajoutPromo

--- src/AdministrateurBundle/Controller/GestionPromotionController.php
+++ src/AdministrateurBundle/Controller/GestionPromotionController.php
@@ -17,9 +17,48 @@ class GestionPromotionController extends Controller
         return $this->render('AdministrateurBundle:Default:gestion_promo.html.twig');
     }
 
-    public function ajoutPromoAction()
+    /**
+     * Fonction qui sert à ajouter une promotion
+     * @param Request $request
+     * @return ...
+     */
+    public function ajoutPromoAction(Request $request)
     {
+        //Affichage de la vue ajout_promotion.html.twig
         return $this->render('AdministrateurBundle:Default:ajout_promotion.html.twig');
+        //Autorisation d'accès de ?? pour les admins
+        $this->denyAccessUnlessGranted(array('ROLE_ADMIN'));
+
+        //$user contient ??
+        $user = $this->getUser();
+
+        // Check if promo don't exist to add this promo
+        //$promos = $this->getDoctrine()->getRepository('ConnexionBundle:Promotion')->find($promo);
+
+        //$lesEnseignants contient les utilisateurs qui ont le rôle Enseignant
+        $lesEnseignants = $this->getDoctrine()->getRepository('ConnexionBundle:User')->findByRole('ROLE_ENSEIGNANT');
+
+        //l'objet $form est créé avec ... ??
+        $form = $this->createForm(new MatiereType($lesEnseignants), $matiere);
+
+        //code qui vérifie et balance tout dans la bdd j'imagine ;)
+        $form->handleRequest($request);
+        if ($form->isSubmitted()) {
+            if ($form->isValid()) {
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($matiere);
+                $em->flush();
+
+                return $this->redirect($this->generateUrl("gerer_promotion"));
+
+            } else
+                $this->addFlash('error', "Tous les champs doivent être complétés.");
+        }
+
+        //Affichage de la vue ajout_promotion.html.twig
+        return $this->render('AdministrateurBundle:Default:ajout_promotion.html.twig', array(
+            'form' => $form->createView(),
+        ));
     }
 
     public function ajoutEtudiantAction()
