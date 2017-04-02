0c451a7c3730c76f16ac05c770464718a9a63274
melninie <twybow91@gmail.com>
2017-04-02 14:33:00 +0200

    Ajout d'un membre du personnel

--- src/AdministrateurBundle/Controller/GestionPersonnelController.php
+++ src/AdministrateurBundle/Controller/GestionPersonnelController.php
@@ -3,6 +3,8 @@
 namespace AdministrateurBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use AdministrateurBundle\Form\PersonnelType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class GestionPersonnelController extends Controller
@@ -13,6 +15,40 @@ class GestionPersonnelController extends Controller
 
         return $this->render('AdministrateurBundle:Default:liste_personnel.html.twig', array(
             'liste_personnel' => $liste_personnel
+        ));
+    }
+
+    public function ajoutPersonnelAction(Request $request)
+    {
+        $this->denyAccessUnlessGranted(array('ROLE_ADMIN'));
+
+        $userManager = $this->container->get('fos_user.user_manager');
+        $personnel = $userManager->createUser();
+
+        $form = $this->createForm(new PersonnelType(), $personnel);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted()) {
+            if ($form->isValid()) {
+                $username = substr($personnel->getPrenom(),0,1)."".substr($personnel->getNom(),0,strlen($personnel->getNom()));
+
+                $personnel->setUsername($username);
+                $personnel->setPlainPassword($username);
+                $personnel  ->setEnabled(true);
+
+                $em = $this->getDoctrine()->getManager();
+                $userManager->updateUser($personnel, true);
+                //$em->persist($personnel);
+                $em->flush();
+
+                return $this->redirect($this->generateUrl("gerer_personnel"));
+
+            } else
+                $this->addFlash('error', "Tous les champs doivent être complétés.");
+        }
+
+        return $this->render('AdministrateurBundle:Default:ajout_personnel.html.twig', array(
+            'form' => $form->createView()
         ));
     }
 }
---
+++ src/AdministrateurBundle/Form/PersonnelType.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace AdministrateurBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use ConnexionBundle\Entity\Metiere;
+
+class PersonnelType extends AbstractType {
+
+    public function buildForm(FormBuilderInterface $builder, array $options) {
+        $builder
+            ->add('nom', 'text', array(
+                "label" => "Nom :"
+            ))
+            ->add('prenom', 'text', array(
+                "label" => "Prénom :"
+            ))
+            ->add('email', 'email', array(
+                "label" => "Email :",
+                'required' => true,
+            ))
+            ->add('tel', 'text', array(
+                "label" => "Téléphone :",
+                'required' => false,
+            ))
+            ->add('roles', 'choice', array(
+                'choices' => array(
+                    'ROLE_ENSEIGNANT' => 'Enseignant',
+                    'ROLE_RESPONSABLE' => 'Responsable',
+                    'ROLE_CFA' => 'CFA',
+                    'ROLE_SECRETAIRE' => 'Secrétaire',
+                    'ROLE_ADMIN' => 'Administrateur',
+                ),
+                'multiple' => true,
+                'expanded' => true,
+                "label" => "Rôle(s) :",
+                'required' => true,
+            ));
+    }
+
+    public function getName() {
+        return 'user';
+    }
+
+    public function configureOptions(OptionsResolver $resolver) {
+        $resolver->setDefaults(array(
+            'data_class' => 'ConnexionBundle\Entity\User',
+        ));
+    }
+
+}
