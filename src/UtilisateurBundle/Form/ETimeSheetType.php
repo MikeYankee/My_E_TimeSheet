65299bcbe26bf7be3348a014f86fd00bb377268c
melninie <twybow91@gmail.com>
2017-04-05 08:29:03 +0200

    Début création ETS

---
+++ src/UtilisateurBundle/Form/ETimeSheetType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace UtilisateurBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use UtilisateurBundle\Form\CoursType;
+
+class ETimeSheetType extends AbstractType {
+
+    public function __construct($horaires, $enseignants, $matieres, $type) {
+        $this->horaires = $horaires;
+        $this->enseignants = $enseignants;
+        $this->matieres = $matieres;
+        $this->type = $type;
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options) {
+        $builder->add('lesCours', CollectionType::class, array(
+            'entry_type' => new CoursType($this->horaires, $this->enseignants, $this->matieres, $this->type)
+        ));
+    }
+
+    public function getName() {
+        return 'cours';
+    }
+
+    public function configureOptions(OptionsResolver $resolver) {
+        $resolver->setDefaults(array(
+            'data_class' => 'ConnexionBundle\Entity\ETimeSheet',
+        ));
+    }
+
+}
